9231ed421af703a912e3912fa96e5700def4d50b
Caleb Heuer <ch@raiguard.me>
2024-08-12 13:10:13 -0600

    Testing criterion

--- scripts/find_sources.sh
+++ scripts/find_sources.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env sh
 
 for name in $(find src -type f -path *.cpp); do
-  echo "$name"
+  if [ -z "$1" ] || [ "$name" != "src/Main.cpp" ]; then
+    echo "$name"
+  fi
 done
+
+if [ -n "$1" ]; then
+  for name in $(find tests -type f -path *.cpp); do
+    echo "$name"
+  done
+fi
---
+++ tests/TestTest.cpp
@@ -0,0 +1,27 @@
+#include <cassert>
+#include <criterion/criterion.h>
+
+Test(simple, test1) {
+  cr_assert(1 != 100);
+}
+
+Test(simple, test2) {
+  cr_assert(2 != 100);
+}
+
+Test(simple, test3) {
+  cr_assert(3 != 100);
+}
+
+Test(simple, test4) {
+  cr_assert(4 != 100);
+}
+
+Test(simple, test5) {
+  cr_assert(5 != 100);
+}
+
+Test(simple, test6) {
+  cr_assert(0);
+}
+
